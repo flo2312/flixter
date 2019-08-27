a8610d09b2c9f6b2150392ce6acd60754ca9e5ee
Matthew Flores <flo2312@gmail.com>
2019-08-27 01:28:54 +0000

    WK9LS25COMPLETE

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
   config.aws_acl    = "public-read"
 
   config.aws_credentials = {
-      access_key_id:     ENV["AWS_ACCESS_KEY"],
-      secret_access_key: ENV["AWS_SECRET_KEY"],
-      region:            ENV["AWS_REGION"]
+    access_key_id:     ENV["AWS_ACCESS_KEY"],
+    secret_access_key: ENV["AWS_SECRET_KEY"],
+    region:            ENV["AWS_REGION"]
   }
 end
