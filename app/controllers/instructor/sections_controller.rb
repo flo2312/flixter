ee42cd76e4c440bea5b32bb1a78da2ed2afb5754
Matthew Flores <flo2312@gmail.com>
2019-08-28 05:07:06 +0000

    WK10LS32INCOMPLETE

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:update]
 
   def new
     @section = Section.new
@@ -9,6 +10,11 @@ class Instructor::SectionsController < ApplicationController
   def create
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
+  end
+
+  def update
+    current_section.update_attributes(section_params)
+    render plain: 'updated!'
   end
 
   private
@@ -20,11 +26,15 @@ class Instructor::SectionsController < ApplicationController
   end
 
   helper_method :current_course
-  def current_course
-    @current_course ||= Course.find(params[:course_id])
-  end
+    def current_course
+      if params[:course_id]
+        @current_course ||= Course.find(params[:course_id])
+      else
+        current_section.course
+      end
+    end
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 end
