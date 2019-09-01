6a5f0d4f907feaba5917f022b7fef9468774aefb
Matthew Flores <flo2312@gmail.com>
2019-09-01 16:41:58 +0000

    WK10LS34INCOMPLETE

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -19,20 +19,33 @@ class Instructor::SectionsController < ApplicationController
 
   private
 
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
   def require_authorized_for_current_course
     if current_course.user != current_user
       render plain: "Unauthorized", status: :unauthorized
     end
   end
 
-  helper_method :current_course
-    def current_course
-      if params[:course_id]
-        @current_course ||= Course.find(params[:course_id])
-      else
-        current_section.course
-      end
+  helper_method :current_section
+  def current_section
+    if params[:id]
+      @current_section ||= Section.find(params[:id])
     end
+  end
+
+  helper_method :current_course
+  def current_course
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
+  end
 
   def section_params
     params.require(:section).permit(:title, :row_order_position)
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,25 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
     
   def show
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section), alert: "You must be enrolled to view lesson!"
+    end
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_lesson.section.course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
 end
+
+
